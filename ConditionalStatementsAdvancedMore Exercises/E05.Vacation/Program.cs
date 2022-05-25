4cdded709f00ce179fb4f73930955c248241001a
Kristian Mladenov <102831846+Krs7N@users.noreply.github.com>
2022-05-25 22:50:27 +0300

    CSharp-Programming-Basics-Conditional-Statements

---
+++ ConditionalStatementsAdvancedMore Exercises/E05.Vacation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace E05.Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal budget = decimal.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string placeToStay = string.Empty;
+            string vacation = string.Empty;
+            if (budget <= 1000)
+            {
+                placeToStay = "Camp";
+                switch (season)
+                {
+                    case "Summer":
+                        budget *= 0.65m;
+                        vacation = "Alaska";
+                        break;
+                    case "Winter":
+                        budget *= 0.45m;
+                        vacation = "Morocco";
+                        break;
+                }
+            }
+            else if (budget <= 3000)
+            {
+                placeToStay = "Hut";
+                switch (season)
+                {
+                    case "Summer":
+                        budget *= 0.8m;
+                        vacation = "Alaska";
+                        break;
+                    case "Winter":
+                        budget *= 0.6m;
+                        vacation = "Morocco";
+                        break;
+                }
+            }
+            else
+            {
+                placeToStay = "Hotel";
+                switch (season)
+                {
+                    case "Summer":
+                        budget *= 0.9m;
+                        vacation = "Alaska";
+                        break;
+                    case "Winter":
+                        budget *= 0.9m;
+                        vacation = "Morocco";
+                        break;
+                }
+            }
+
+            Console.WriteLine($"{vacation} - {placeToStay} - {budget:f2}");
+        }
+    }
+}
