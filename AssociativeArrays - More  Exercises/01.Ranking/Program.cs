a0deb9f1ce61cecd0b41256cc8812e9f2aefaf7c
Kristian Mladenov <krstntank@gmail.com>
2022-07-28 19:16:58 +0300

    Added first two problems from More Exercises

---
+++ AssociativeArrays - More  Exercises/01.Ranking/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> passwordsByContests = new Dictionary<string, string>();
+            Dictionary<string, Dictionary<string, int>> results = new Dictionary<string, Dictionary<string, int>>();
+
+            string[] contestInfo = Console.ReadLine().Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+            while (contestInfo[0] != "end of contests")
+            {
+                string contest = contestInfo[0];
+                string password = contestInfo[1];
+
+                passwordsByContests.Add(contest, password);
+
+                contestInfo = Console.ReadLine().Split(':', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            string[] submissionInfo = Console.ReadLine().Split("=>", StringSplitOptions.RemoveEmptyEntries);
+
+            while (submissionInfo[0] != "end of submissions")
+            {
+                string submissionContest = submissionInfo[0];
+                string submissionPassword = submissionInfo[1];
+                string username = submissionInfo[2];
+                int pointsReceived = int.Parse(submissionInfo[3]);
+
+                if (passwordsByContests.ContainsKey(submissionContest) && passwordsByContests[submissionContest] == submissionPassword)
+                {
+                    if (!results.ContainsKey(username))
+                    {
+                        results[username] = new Dictionary<string, int>();
+                    }
+
+                    if (results.ContainsKey(username) && !results[username].ContainsKey(submissionContest))
+                    {
+                        results[username][submissionContest] = 0;
+                    }
+
+                    if (results[username][submissionContest] < pointsReceived)
+                    {
+                        results[username][submissionContest] = pointsReceived;
+                    }
+                }
+
+                submissionInfo = Console.ReadLine().Split("=>", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            string winner = results.OrderBy(x => x.Value.Values.Sum()).Last().Key;
+            int bestPoints = results.OrderBy(x => x.Value.Values.Sum()).Last().Value.Values.Sum();
+
+            Console.WriteLine($"Best candidate is {winner} with total {bestPoints} points.");
+            Console.WriteLine("Ranking:");
+
+            foreach (var item in results.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(item.Key);
+                foreach (var contest in item.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ AssociativeArrays - More  Exercises/02.Judge/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contestByUsernameAndPoints = new Dictionary<string, Dictionary<string, int>>();
+            var usernameByTotalPoints = new SortedDictionary<string, int>();
+
+            string[] judgeInfo = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (judgeInfo[0] != "no more time")
+            {
+                string username = judgeInfo[0];
+                string contest = judgeInfo[1];
+                int points = int.Parse(judgeInfo[2]);
+
+                if (!contestByUsernameAndPoints.ContainsKey(contest))
+                {
+                    contestByUsernameAndPoints.Add(contest, new Dictionary<string, int>());
+                    contestByUsernameAndPoints[contest].Add(username, points);
+                }
+                else
+                {
+                    if (!contestByUsernameAndPoints[contest].ContainsKey(username))
+                    {
+                        contestByUsernameAndPoints[contest].Add(username, points);
+                    }
+                    else
+                    {
+                        if (contestByUsernameAndPoints[contest][username] < points)
+                        {
+                            contestByUsernameAndPoints[contest][username] = points;
+                        }
+                    }
+                }
+
+                judgeInfo = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var contest in contestByUsernameAndPoints)
+            {
+                Console.WriteLine($"{contest.Key}: {contest.Value.Count} participants");
+                int placement = 1;
+                foreach (var participant in contest.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{placement}. {participant.Key} <::> {participant.Value}");
+                    placement++;
+                }
+            }
+
+            int placementByTotalPoints = 1;
+            Console.WriteLine("Individual standings:");
+
+            foreach (var contest in contestByUsernameAndPoints)
+            {
+                foreach (var user in contest.Value)
+                {
+                    if (!usernameByTotalPoints.ContainsKey(user.Key))
+                    {
+                        usernameByTotalPoints.Add(user.Key, user.Value);
+                    }
+                    else
+                    {
+                        usernameByTotalPoints[user.Key] += user.Value;
+                    }
+                }
+            }
+
+            foreach (var participant in usernameByTotalPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{placementByTotalPoints}. {participant.Key} -> {participant.Value}");
+                placementByTotalPoints++;
+            }
+        }
+    }
+}
